5b134265616b6070734b67c094ba95524d054173
Oscar Berg <oscar.berg@zoho.com>
2016-08-30 15:26:09 +0200

    Add files via upload

---
+++ curl_twitch_v2.ps1
@@ -0,0 +1,16 @@
+﻿$var = curl.exe --insecure https://api.twitch.tv/kraken/streams
+$data = $var | ConvertFrom-Json
+
+$lists = @("World of Warcraft","Dota 2")
+
+Function Get-Stream($game){
+    $var = $data.streams | where {$_.game -eq $game}
+
+    $totalviewers = ($var.viewers | Measure-Object -sum).sum
+
+return "$game;$totalviewers"
+}
+
+foreach($list in $lists){
+    Get-Stream $list
+}
