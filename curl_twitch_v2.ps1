126914180c0ff5d208d2ec4e4ab105d43aa87be3
Oscar Berg <oscar.berg@zoho.com>
2016-09-06 16:05:04 +0200

    Add files via upload

--- curl_twitch_v2.ps1
+++ curl_twitch_v2.ps1
@@ -1,16 +1,35 @@
-﻿$var = curl.exe --insecure https://api.twitch.tv/kraken/streams
-$data = $var | ConvertFrom-Json
+﻿Function Get-StreamGame{ 
+    $curl = curl.exe --insecure --silent https://api.twitch.tv/kraken/streams
+    $data = $curl | ConvertFrom-Json
 
-$lists = @("World of Warcraft","Dota 2")
-
-Function Get-Stream($game){
-    $var = $data.streams | where {$_.game -eq $game}
-
-    $totalviewers = ($var.viewers | Measure-Object -sum).sum
-
-return "$game;$totalviewers"
+    $games = $data.streams.game | Sort-Object -Unique
+    $games = $games | ? {$_}
+    foreach($games in $games){
+        $var = $data.streams | where {$_.game -eq $games}
+        $totalviewers = ($var.viewers | Measure-Object -sum).sum
+        Write-Host "$games;$totalviewers"
+    }
 }
 
-foreach($list in $lists){
-    Get-Stream $list
+Function Get-Streamers{
+    $curl = curl.exe --insecure --silent https://api.twitch.tv/kraken/streams
+    $data = $curl | ConvertFrom-Json
+
+    $s = $data.streams._links.count
+    $s = $s - 1
+    $i = 0
+    while($i -le $s){
+        
+        $streamer = $data.streams | where {$_._links.self -eq $data.streams._links.self[$i]}
+        $name = $streamer.channel.name
+        $game = $streamer.channel.game
+        $viewers = $streamer.viewers
+        $followers = $streamer.channel.followers
+
+        write-host "$name;$game;$viewers;$followers"
+        $i++
+    }
 }
+
+Get-StreamGame
+Get-Streamers
